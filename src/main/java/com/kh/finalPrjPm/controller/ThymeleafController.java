a61a7b15345ef3031942cc48274efdb89dcc4653
woojin <sun65568@hanmail.net>
2023-06-16 19:34:38 +0900

    스프링 연습

---
+++ src/main/java/com/kh/finalPrjPm/controller/ThymeleafController.java
@@ -0,0 +1,51 @@
+package com.kh.finalPrjPm.controller;
+
+import com.kh.finalPrjPm.dto.ItemDto;
+import com.kh.finalPrjPm.entity.Item;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafController {
+    @GetMapping("/item")
+    public String thymeleafItem(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setName("LG 오브젝 에어컨");
+        itemDto.setDetail("LG 23년형 에어컨 입니다.");
+        itemDto.setPrice(100000);
+        itemDto.setRegTime(LocalDateTime.now());
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafItem";
+    }
+    @GetMapping("/item-list")
+    public String thymeleafItemList(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for(int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setName("LG 오브젝 에어컨");
+            itemDto.setDetail("LG 23년형 에어컨 입니다.");
+            itemDto.setPrice(100000);
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafItemList";
+    }
+    @GetMapping("/link-move")
+    public String thymeleafLinkMove() {
+        return "thymeleafEx/thymeleafLinkMove";
+    }
+    @GetMapping("/link-value")
+    public String thymeleafLinkValue(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafLinkParam";
+    }
+}
--- src/main/java/com/kh/finalPrjPm/dto/ItemDto.java
+++ src/main/java/com/kh/finalPrjPm/dto/ItemDto.java
@@ -3,10 +3,13 @@ package com.kh.finalPrjPm.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter @Setter
 public class ItemDto {
     private String name;
     private int price;
     private String detail;
     private int stock;
+    private LocalDateTime regTime;
 }
